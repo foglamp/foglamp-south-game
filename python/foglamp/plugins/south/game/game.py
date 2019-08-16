e1d9655de80b51d183eea313ab8e2f9e9ca2fef0
Singhal-Vaibhav <vaibhav@scaledb.com>
2019-08-16 13:18:55 -0400

    plugin info version bumped

--- python/foglamp/plugins/south/game/game.py
+++ python/foglamp/plugins/south/game/game.py
@@ -59,7 +59,7 @@ def plugin_info():
 
     return {
         'name': 'IoT Lab Game Plugin',
-        'version': '1.5.0',
+        'version': '1.7.0',
         'mode': 'poll',
         'type': 'south',
         'interface': '1.0',
